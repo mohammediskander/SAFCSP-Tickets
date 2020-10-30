9d4feb1db6e59cf5b3b7c449cef2358d93b1d007
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-10-30 23:15:00 +0300

    Add Ticket Struct

--- Tickets/Tickets/ViewControllers/AddTicketsController.swift
+++ Tickets/Tickets/ViewControllers/AddTicketsController.swift
@@ -10,8 +10,13 @@ import UIKit
 class AddTicketsController: UIViewController {
     
     
-    let letters = "A"..."z"
-    let numbers = 0...9
+    let ticketTypes = [
+        "Food",
+        "Gift",
+        "Other",
+        "Personal Product",
+        "Supermarket"
+    ]
 
     
     @IBOutlet var ticketNameField: UITextField!
@@ -34,6 +39,15 @@ class AddTicketsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePicker.minimumDate = Date()
+    }
+    
+    
+    @IBAction func addTicketButtonPressed(_ sender: UIButton) {
+        
+        
+        
+        
     }
 }
 
@@ -43,6 +57,9 @@ extension AddTicketsController: UITextFieldDelegate, UITextPasteDelegate {
     
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let letters = "A"..."z"
+        let numbers = 0...9
         
         guard string != "" else {
             return true
@@ -74,7 +91,7 @@ extension AddTicketsController: UITextFieldDelegate, UITextPasteDelegate {
                 return false
 
             }
-            
+            moveCursor(textField,withText: string)
             return true
         }
         
@@ -90,10 +107,94 @@ extension AddTicketsController: UITextFieldDelegate, UITextPasteDelegate {
             guard letters.contains(string) else {
                 return false
             }
-                        
+            
+            moveCursor(textField,withText: string)
             return true
         }
         
+        moveCursor(textField,withText: string)
         return true
     }
+    
+    func moveCursor(_ textField: UITextField,withText text: String) {
+        
+        if textField == carsLicensePlateLettersTextField1{
+            carsLicensePlateLettersTextField2.becomeFirstResponder()
+        }
+        if textField == carsLicensePlateLettersTextField2{
+            carsLicensePlateLettersTextField3.becomeFirstResponder()
+        }
+        if textField == carsLicensePlateLettersTextField3{
+            carsLicensePlateNumbersTextField1.becomeFirstResponder()
+        }
+        if textField == carsLicensePlateNumbersTextField1{
+            carsLicensePlateNumbersTextField2.becomeFirstResponder()
+        }
+        if textField == carsLicensePlateNumbersTextField2{
+            carsLicensePlateNumbersTextField3.becomeFirstResponder()
+        }
+        if textField == carsLicensePlateNumbersTextField3{
+            carsLicensePlateNumbersTextField4.becomeFirstResponder()
+        }
+        if textField == carsLicensePlateNumbersTextField4{
+            textField.resignFirstResponder()
+        }
+        
+        textField.text = text.uppercased()
+        
+    }
+}
+
+extension AddTicketsController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ticketTypes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ticketTypes[row]
+    }
+}
+
+enum TicketType: String, Codable {
+    case food            = "Food"
+    case gift            = "Gift"
+    case other           = "Other"
+    case personalProduct = "Personal Product"
+    case supermarket     = "Supermarket"
+    
+    static let types = [food,gift, other, personalProduct, supermarket]
+    
+    init?(id : Int) {
+            switch id {
+            case 0:
+                self = .food
+            case 1:
+                self = .gift
+            case 2:
+                self = .other
+            case 3:
+                self = .personalProduct
+            case 4:
+                self = .supermarket
+            default:
+                return nil
+            }
+        }
+}
+
+
+struct Ticket: Codable {
+    let ticketName: String
+    let ticketType: TicketType
+    let expectedDeliveryDate: Date
+    let DriversName: String
+    let CarsModel: String
+    let licencePlate: String
+    let carscolor: String
+
+
 }
