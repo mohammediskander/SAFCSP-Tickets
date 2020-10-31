a402fc62c2f789da87d13cf3031cb31826688439
Mohammed Eskander <idarkcry@icloud.com>
2020-10-31 15:41:09 +0300

    Create textfield

---
+++ Tickets/Tickets/Views/TextField.swift
@@ -0,0 +1,18 @@
+//
+//  TextField.swift
+//  Tickets
+//
+//  Created by Mohammed Iskandar on 31/10/2020.
+//
+
+import UIKit
+
+class TextField: UITextField {
+    override init(frame: CGRect) {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        <#code#>
+    }
+}
